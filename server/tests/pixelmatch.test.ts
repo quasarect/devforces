0a61fde4bd76c7fae6926e84808b6df4a4db723b
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2024-09-11 19:31:35 +0530

    added 9 test cases for image comparison

--- server/tests/pixelmatch.test.ts
+++ server/tests/pixelmatch.test.ts
@@ -11,6 +11,8 @@ const matcher = (image1: string, image2: string) => {
 	);
 };
 
+//image 1
+
 test("pixelmatch 0", () => {
 	expect(matcher("user_response_1_image_1.png", "generated_image_1.png")).toBe(
 		"95.8",
@@ -18,13 +20,53 @@ test("pixelmatch 0", () => {
 });
 
 test("pixelmatch 1", () => {
-	expect(matcher("user_response_1_image_1.png", "generated_image_2.png")).toBe(
-		"95.8",
+	expect(matcher("user_response_2_image_1.png", "generated_image_1.png")).toBe(
+		"97.6",
 	);
 });
 
-test("pixelmatch 1", () => {
-	expect(matcher("user_response_1_image_1.png", "generated_image_3.png")).toBe(
-		"95.8",
+test("pixelmatch 2", () => {
+	expect(matcher("user_response_3_image_1.png", "generated_image_1.png")).toBe(
+		"94.1",
+	);
+});
+
+//image 2
+
+test("pixelmatch 3", () => {
+	expect(matcher("user_response_1_image_2.png", "generated_image_2.png")).toBe(
+		"83.2",
+	);
+});
+
+test("pixelmatch 4", () => {
+	expect(matcher("user_response_2_image_2.png", "generated_image_2.png")).toBe(
+		"81.7",
+	);
+});
+
+test("pixelmatch 5", () => {
+	expect(matcher("user_response_3_image_2.png", "generated_image_2.png")).toBe(
+		"89.1",
+	);
+});
+
+//image 3
+
+test("pixelmatch 6", () => {
+	expect(matcher("user_response_1_image_3.png", "generated_image_3.png")).toBe(
+		"93.7",
+	);
+});
+
+test("pixelmatch 7", () => {
+	expect(matcher("user_response_2_image_3.png", "generated_image_3.png")).toBe(
+		"70.8",
+	);
+});
+
+test("pixelmatch 8", () => {
+	expect(matcher("user_response_3_image_3.png", "generated_image_3.png")).toBe(
+		"79.7",
 	);
 });
