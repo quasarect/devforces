38cee1582cd8827255f6b6f638ab08f1bb75a9d8
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-09-16 19:45:20 +0530

    Updated Front-End Pages , Reusable components in Components , Pages in Pages section

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App.tsx';
+import CSSBattles from './pages/CSSBattles.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path : '/CSS-Battles',
+    element: <CSSBattles />,
   },
 ]);
 
