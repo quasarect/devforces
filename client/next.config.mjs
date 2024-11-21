4dca64960a6f6af1825f66ad53f60bcea3690cbf
Pratik Pakhale <quasarect@gmail.com>
2024-11-22 04:12:43 +0530

    rm

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
     ],
   },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
